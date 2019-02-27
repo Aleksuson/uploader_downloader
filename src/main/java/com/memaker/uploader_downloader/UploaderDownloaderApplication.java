e41c1a9ac3f4c541358bbadf3ba27fefea0eb881
MakeMe <aleksander.e.popiolek@gmail.com>
2019-02-27 14:55:52 +0100

    Added exceptions, controller, services and payload

--- src/main/java/com/memaker/uploader_downloader/UploaderDownloaderApplication.java
+++ src/main/java/com/memaker/uploader_downloader/UploaderDownloaderApplication.java
@@ -1,11 +1,12 @@
 package com.memaker.uploader_downloader;
 
+import com.memaker.uploader_downloader.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties
+@EnableConfigurationProperties({FileStorageProperties.class})
 public class UploaderDownloaderApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/memaker/uploader_downloader/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.memaker.uploader_downloader.exception;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
